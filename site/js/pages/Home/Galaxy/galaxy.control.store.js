d90d001e138afaeadd5f6e538d370b107b15f7d1
David delhart <dedelhart@voicebase.com>
2020-02-23 20:03:51 -0800

    basic radial drag - neeeds a thumb

--- site/js/pages/Home/Galaxy/galaxy.control.store.js
+++ site/js/pages/Home/Galaxy/galaxy.control.store.js
@@ -22,6 +22,11 @@ PIXI.Loader.shared.load((loader, resources) => {
   imgResources = resources;
 });
 
+const ptDist = (p1, p2) => Math.sqrt(
+  ((p1.x - p2.x) ** 2)
+    + ((p1.y - p2.y) ** 2),
+);
+
 export default (galaxyStream, size) => {
   const stream = pixiStreamFactory({ size })
     .property('galaxyParts', [], 'array')
@@ -46,9 +51,9 @@ export default (galaxyStream, size) => {
       s.do.setPartAxisAnchor(new PIXI.Container());
       s.do.setPartRadiusAnchor(new PIXI.Container());
       s.my.anchor.addChild(s.my.partTabAnchor);
+      s.my.anchor.addChild(s.my.partRadiusAnchor);
       s.my.anchor.addChild(s.my.partAxisAnchor);
       s.my.anchor.addChild(s.my.densityAnchor);
-      s.my.anchor.addChild(s.my.partRadiusAnchor);
       s.my.app.stage.addChild(s.my.anchor);
       s.do.centerAnchor();
     })
@@ -101,7 +106,10 @@ export default (galaxyStream, size) => {
       return graphics;
     })
     .method('lyToPx', (s) => _N(s.do.radius()).div(galaxyStream.my.chosenGalaxy.d).value)
-    .method('makeCenterButton', (s, part) => {
+    .method('makeCenterButton', (s) => {
+      // @TODO: reuse graphic
+      const part = s.do.activePart();
+      if (!part) return;
       const graphics = new PIXI.Graphics();
 
       const PART_AXIS_RADIUS = s.do.radius() / 20;
@@ -116,9 +124,7 @@ export default (galaxyStream, size) => {
         .lineTo(0, -PART_AXIS_RADIUS);
 
       const scale = s.do.lyToPx();
-
-      graphics.x = scale * (part.x || 0);
-      graphics.y = scale * (part.y || 0);
+      s.do.centerGraphicsToPart(graphics, part);
 
       const onDone = ({ x, y }) => {
         galaxyStream.do.updatePartPos(part, x / scale, y / scale);
@@ -126,32 +132,118 @@ export default (galaxyStream, size) => {
       };
 
       const onMove = () => {
-        const radGraphic = s.my.radiusContainer.getChildAt(0)
-        if (radGraphic){
+        if (_.get(s, 'my.partRadiusAnchor.children.length', 0)) {
+          const radGraphic = s.my.partRadiusAnchor.getChildAt(0);
           radGraphic.x = graphics.x;
           radGraphic.y = graphics.y;
         }
-      }
+      };
 
       s.do.makeDraggable(graphics, onMove, onDone);
-
       return graphics;
     })
-    .method('makeRadialButton', (s, part) => {
-      const graphics = new PIXI.Graphics();
+    .method('partLocation', (s) => {
+      // this is the location of the graphics REPRESENTING the active part
+      // RELATIVE to the partAxis container.
+      const i = _.get('s.my.partAxisAnchor.length', 0);
+      if (i < 1) {
+        return null;
+      }
+      const graphics = _.first(s.my.partAxisAnchor.children);
+      if (!graphics) return null;
+      return { x: graphics.x, y: graphics.y };
+    })
+    .method('findRadiusGraphic', (s) => {
+      const i = _.get('s.my.partRadiusAnchor.length', 0);
+      return i ? _.first(s.my.partRadiusAnchor.children) : null;
+    })
+    .method('findAxisGraphic', (s) => {
+      const i = _.get('s.my.partAxisAnchor.length', 0);
+      return i ? _.first(s.my.partAxisAnchor.children) : null;
+    })
+    .method('makeRBdraggable', (s) => {
+      const graphics = s.do.findRadiusGraphic();
+      if (!graphics) return;
+      let dragStart;
+      let dragStartPos;
+      let dragStartRadius = 0;
+      let dragCurrentRadius = 0;
+
+      const startDrag = (e) => {
+        dragStart = e.data;
+        dragStartPos = dragStart.getLocalPosition(graphics.parent);
+        try {
+          const ag = s.do.findAxisGraphic();
+          dragStartRadius = ptDist(dragStartPos, ag.position);
+          console.log('start dragging, pos', dragStartPos, 'radius', dragStartRadius);
+        } catch (err) {
+          console.log('error: ', err);
+        }
+      };
+
+      // as we drag we temporarily scale the graphics for efficiencies' sake.
+      const drag = () => {
+        if (!dragStart) return;
+
+        const dragPos = dragStart.getLocalPosition(graphics.parent);
+        const ag = s.do.findAxisGraphic();
+        dragCurrentRadius = ptDist(dragPos, ag.position);
+        if (dragStartRadius && dragCurrentRadius) {
+          const dragScale = dragCurrentRadius / dragStartRadius;
+          console.log('scaling graphics to ', dragScale);
+          graphics.scale = { x: dragScale, y: dragScale };
+        } else {
+          console.log('bad radii:', dragStartRadius, '/current', dragCurrentRadius);
+        }
+      };
+      const stopDrag = () => {
+        dragStart = null;
+        // TODO: change radius!
+      };
+
+      graphics.on('mousedown', startDrag);
+      graphics.on('mousemove', drag);
+      graphics.on('mouseup', stopDrag);
+    })
+    .method('centerGraphicsToPart', (s, graphics) => {
+      const part = s.do.activePart();
+      if (!part) {
+        return;
+      }
       const scale = s.do.lyToPx();
       graphics.x = scale * (part.x || 0);
       graphics.y = scale * (part.y || 0);
-
-      graphics.lineStyle(4, PART_AXIS_COLOR, 0.5)
-        .drawCircle(0, 0, part.diameter/2 * scale);
-
-      return graphics;
     })
-    .method('redrawRadius', (s, part) => {
-      s.my.partRadiusAnchor.removeChildren();
-      if (!part.diameter) return;
-      s.my.partRadiusAnchor.addChild(s.do.makeRadialButton(part));
+    .method('makeRadialButton', (s) => {
+      const part = s.do.activePart();
+      if (!part) {
+        return;
+      }
+      let graphics = s.do.findRadiusGraphic();
+      if (graphics && graphics instanceof PIXI.Graphics) {
+        graphics.clear();
+      } else {
+        graphics = new PIXI.Graphics();
+        graphics.interactive = true;
+        s.my.partRadiusAnchor.addChild(graphics);
+        s.do.makeRBdraggable();
+      }
+      s.do.centerGraphicsToPart(graphics);
+      const scale = s.do.lyToPx();
+      graphics.beginFill(BLACK, 0.1)
+        .drawCircle(0, 0, (part.diameter / 2) * scale)
+        .endFill();
+
+      graphics.lineStyle(1, PART_AXIS_COLOR, 1)
+        .drawCircle(0, 0, (part.diameter / 2) * scale);
+    })
+    .method('redrawRadius', (s) => {
+      const part = s.do.activePart();
+      if (!_.get(part, 'diameter', 0)) {
+        s.my.partRadiusAnchor.removeChildren();
+        return;
+      }
+      s.do.makeRadialButton();
     })
     .method('drawActivePart', (s) => {
       if (!s.my.partAxisAnchor) {
@@ -163,7 +255,7 @@ export default (galaxyStream, size) => {
         return;
       }
 
-      s.my.partAxisAnchor.addChild(s.do.makeCenterButton(part));
+      s.my.partAxisAnchor.addChild(s.do.makeCenterButton());
       s.do.redrawRadius(part);
     })
     .watch('activePartIndex', 'drawActivePart')
@@ -250,6 +342,8 @@ export default (galaxyStream, size) => {
       }
     })
     .method('updateDensity', (s, val) => {
+      // NOTE - this method SENDS a change command to the galaxyStream ValueStream.
+      // this UI listens to changes in parts, so indirectly should reflect these parts.
       const part = s.do.activePart();
       if (part) {
         galaxyStream.do.updatePartDensity(part, val);
